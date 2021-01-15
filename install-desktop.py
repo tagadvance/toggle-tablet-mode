dde6025eb4df649e0cc0b0ce2eeebf5ab85dc0c0
Tag <tagadvance+github.com@gmail.com>
2021-01-15 01:40:07 -0700

    add rotation and inversion

--- install-desktop.py
+++ install-desktop.py
@@ -3,6 +3,7 @@
 import os, stat
 
 dir_path = os.path.dirname(os.path.realpath(__file__))
+
 path = os.path.expanduser('~/.local/share/applications/toggle.desktop')
 with open(path, 'w') as w:
 	w.write("""\
@@ -16,4 +17,51 @@ Name=Toggle Tablet Mode
 Comment=Toggle Tablet Mode
 Icon=%s/toggle.png
 """ % (dir_path, dir_path))
-os.chmod(path, stat.S_IREAD | stat.S_IWRITE);
+os.chmod(path, stat.S_IREAD | stat.S_IWRITE)
+
+path = os.path.expanduser('~/.local/share/applications/toggle-left.desktop')
+with open(path, 'w') as w:
+	w.write("""\
+#!/usr/bin/env xdg-open
+[Desktop Entry]
+Version=1.0
+Type=Application
+Terminal=false
+Exec=%s/toggle-tablet-mode.py left
+Name=Toggle Tablet Mode and Rotate Left
+Comment=Toggle Tablet Mode and Rotate Left
+Icon=%s/toggle-left.png
+""" % (dir_path, dir_path))
+os.chmod(path, stat.S_IREAD | stat.S_IWRITE)
+
+path = os.path.expanduser('~/.local/share/applications/toggle-right.desktop')
+with open(path, 'w') as w:
+	w.write("""\
+#!/usr/bin/env xdg-open
+[Desktop Entry]
+Version=1.0
+Type=Application
+Terminal=false
+Exec=%s/toggle-tablet-mode.py right
+Name=Toggle Tablet Mode and Rotate Right
+Comment=Toggle Tablet Mode and Rotate Right
+Icon=%s/toggle-right.png
+""" % (dir_path, dir_path))
+os.chmod(path, stat.S_IREAD | stat.S_IWRITE)
+
+path = os.path.expanduser('~/.local/share/applications/toggle-inverted.desktop')
+with open(path, 'w') as w:
+	w.write("""\
+#!/usr/bin/env xdg-open
+[Desktop Entry]
+Version=1.0
+Type=Application
+Terminal=false
+Exec=%s/toggle-tablet-mode.py inverted
+Name=Toggle Tablet Mode and Invert Display
+Comment=Toggle Tablet Mode and Invert Display
+Icon=%s/toggle-inverted.png
+""" % (dir_path, dir_path))
+os.chmod(path, stat.S_IREAD | stat.S_IWRITE)
+
+
--- toggle-tablet-mode.py
+++ toggle-tablet-mode.py
@@ -2,36 +2,71 @@
 
 import os
 import re
+from sys import argv
 
 def main() -> None:
-	keyboard_id = get_keyboard()
-	touchpad_id = get_touchpad()
-	toggle_device(keyboard_id)
-	toggle_device(touchpad_id)
+    orientation = None if (len(argv) < 2) else argv[1]
+
+    # toggling from inverted to right should not re-enable the input devices
+    if orientation is None:
+        # default behavior
+        toggle_input()
+        reset_display_orientation()
+    else:
+        # we are already in this orientation: reset OR the keyboard is enabled and needs to be disabled
+        if is_in_orientation(orientation) or is_input_enabled():
+            toggle_input()
+        toggle_display_orientation(orientation)
+
+def is_input_enabled() -> bool:
+    keyboard_id = get_keyboard()
+    return is_enabled(keyboard_id)
+
+def toggle_input() -> None:
+    keyboard_id = get_keyboard()
+    touchpad_id = get_touchpad()
+    toggle_device(keyboard_id)
+    toggle_device(touchpad_id)
 
 def get_keyboard(device: str = 'AT Translated Set 2 keyboard'):
-	meta = run('xinput --list | grep "%s"' % device)
-	m = re.search('id=(\d+)', meta)
-	return m.group(1)
+    meta = run('xinput --list | grep "%s"' % device)
+    m = re.search('id=(\d+)', meta)
+    return m.group(1)
 
 def get_touchpad(device: str = 'Touchpad'):
-	meta = run('xinput --list | grep -i "%s"' % device)
-	m = re.search('id=(\d+)', meta)
-	return m.group(1)
+    meta = run('xinput --list | grep -i "%s"' % device)
+    m = re.search('id=(\d+)', meta)
+    return m.group(1)
 
 def toggle_device(device: str) -> None:
-	state = is_enabled(device)
-	new_state = state ^ 1
-	run('xinput set-prop "%s" "Device Enabled" "%d"' % (device, new_state))
+    state = is_enabled(device)
+    new_state = state ^ 1
+    run('xinput set-prop "%s" "Device Enabled" "%d"' % (device, new_state))
 
 def is_enabled(device: str) -> int:
-	meta = run('xinput list-props "%s" | grep "Device Enabled"' % device)
-	m = re.search(':\s*(\d+)', meta)
-	value = m.group(1)
-	return int(value)
+    meta = run('xinput list-props "%s" | grep "Device Enabled"' % device)
+    m = re.search(':\s*(\d+)', meta)
+    value = m.group(1)
+    return int(value)
+
+def is_in_orientation(orientation: str) -> bool:
+    current_orientation = get_orientation()
+    return current_orientation == orientation
+
+def toggle_display_orientation(orientation: str) -> None:
+    set_display_orientation('normal' if is_in_orientation(orientation) else orientation)
+
+def get_orientation() -> str:
+    return run('xrandr --query --verbose | grep "eDP" | cut -d " " -f 6').strip()
+
+def set_display_orientation(orientation: str) -> None:
+    run('xrandr -o %s' % orientation)
+
+def reset_display_orientation() -> int:
+    set_display_orientation('normal')
 
 def run(command: str) -> str:
-	return os.popen(command).read()
+    return os.popen(command).read()
 
 if __name__ == '__main__':
     main()
